8e0775d9429afa7e9ade4234e28ceef10713f012
Siddharth-22-1994 <siddharthece1994@gmail.com>
2024-01-03 22:04:33 +0530

    2nd commit

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class students(models.Model):
+    name=models.CharField(max_length=40)
+    age=models.IntegerField(max_length=40)
--- herokuproj/settings.py
+++ herokuproj/settings.py
@@ -80,11 +80,16 @@ WSGI_APPLICATION = 'herokuproj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "railway",
+        "USER":"postgres",
+        "PASSWORD": "cdFC3Aae25De56FdefD5112Dcc2*3*f6",
+        "HOST": "viaduct.proxy.rlwy.net",
+        "PORT": "55621"
     }
 }
 
+DATABASES['defaults'] = dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -122,12 +127,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS=(os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS=os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 
 django_heroku.settings(locals())
