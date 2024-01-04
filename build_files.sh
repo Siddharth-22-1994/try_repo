e68d4a6f32d4178f69d02a04d468525f469b782e
Siddharth-22-1994 <siddharthece1994@gmail.com>
2024-01-04 09:08:00 +0530

    9th commit

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
-python3.10 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
-python3.10 manage.py makemigrations --noinput
-python3.10 manage.py migrate --noinput
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
 
-python3.10 manage.py collectstatic --noinput --clear
+python3 manage.py collectstatic --noinput --clear
